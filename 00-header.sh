e1dd59874b438fed68987ceccb8cb7f2019beec4
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2024-01-04 08:19:17 +0800

    header

---
+++ 00-header.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo -e "\033[0;32mWelcome back Mr. Admin... Glad to see you again..... Really!\033[0m"
+echo -e "\033[0;35mWelcome back Mr. Admin... Glad to see you again..... Really!\033[0m"
+echo -e "\033[0;31mWelcome back Mr. Admin... Glad to see you again..... Really!\033[0m"
+echo -e "\033[0;0mWelcome back Mr. Admin... Glad to see you again..... Really!\033[0m"
+echo
