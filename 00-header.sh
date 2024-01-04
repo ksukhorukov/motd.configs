80c7d91765775dbd50425d824bf4ed4407ae4ee8
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2024-01-04 08:23:41 +0800

    indentation from the top

--- 00-header.sh
+++ 00-header.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo
 echo -e "\033[0;32mWelcome back Mr. Admin... Glad to see you again..... Really!\033[0m"
 echo -e "\033[0;35mWelcome back Mr. Admin... Glad to see you again..... Really!\033[0m"
 echo -e "\033[0;31mWelcome back Mr. Admin... Glad to see you again..... Really!\033[0m"
